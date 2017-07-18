1824e7d3fda8cba82abffa7dd21a58a3361d1b67
Jack Cooper <jacksoncooper@users.noreply.github.com>
2017-07-18 15:09:44 -0700

    Add homework 4, exercise 2 and 3.

--- 04-higher-order/homework-4.hs
+++ 04-higher-order/homework-4.hs
@@ -3,6 +3,7 @@
 {-# OPTIONS_GHC -Wall #-}
 
 -- Exercise 1
+-- TODO: That 'filter even' in fun2' is really gross because it only removes the first odd element in the resulting list.
 
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
@@ -21,3 +22,35 @@ fun2 n
 
 fun2' :: Integer -> Integer
 fun2' = sum . filter even . takeWhile (> 1) . iterate (\x -> if even x then x `div` 2 else 3 * x + 1)
+
+-- Exercise 2
+-- TODO: The populate function is a hack that requires an extra traversal of the tree. Find a way to populate height values in the 'insert' function.
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = populate . foldr insert Leaf
+
+insert :: a -> Tree a -> Tree a
+insert a Leaf = Node 0 Leaf a Leaf
+insert a self@(Node _ leftChild b rightChild)
+    | height leftChild <= height rightChild = (Node (height self) (insert a leftChild) b rightChild)
+    | otherwise                             = (Node (height self) leftChild b (insert a rightChild))
+
+height :: Tree a -> Integer
+height Leaf = -1
+height (Node _ leftChild _ rightChild) = succ $ max (height leftChild) (height rightChild)
+
+populate :: Tree a -> Tree a
+populate Leaf = Leaf
+populate self@(Node _ leftChild value rightChild) = (Node (height self) (populate leftChild) value (populate rightChild))
+
+-- Exercise 3
+
+xor :: [Bool] -> Bool
+xor = foldl (/=) False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr ((:) . f) []
