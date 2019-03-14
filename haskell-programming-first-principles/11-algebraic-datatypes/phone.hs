21385efb16e682b2245706cb978517a9e5d1c9ad
jacksoncooper <jacksoncooper@users.noreply.github.com>
2019-03-14 13:59:03 -0400

    Finish Chapter 11 "Phone exercise".

--- haskell-programming-first-principles/11-algebraic-datatypes/phone.hs
+++ haskell-programming-first-principles/11-algebraic-datatypes/phone.hs
@@ -1,12 +1,10 @@
-import Data.Char (isLetter, isUpper, toLower)
+import Data.Char (isLetter, toLower, isUpper)
 import Data.List (elemIndex)
 
 -- Chapter Exercises, p.454
 
 data Phone = Phone [Button]
-
 data Button = Button Char String
-  deriving Show
 
 type Digit = Char
 type Presses = Int
@@ -36,20 +34,24 @@ conversation =
     "Lol ya",
     "Just making sure rofl ur turn" ]
 
+-- Part 1
+
 characterToTaps :: Phone -> Char -> [(Digit, Presses)]
-characterToTaps phone character = go $ getButton phone character
+characterToTaps phone character = go $ getButton phone (toLower character)
   where
     go (Just (Button label sequence)) =
-      case isLetter character && isUpper character of
+      case shouldShift of
         True  -> characterToTaps phone '^' ++ characterToTaps phone (toLower character)
         False -> [(label, getLocationInSequence character (sequence ++ [label]))]
+        where
+          shouldShift = isLetter character && isUpper character
     go Nothing = error ("Unable to translate '" ++ [character] ++ "' to taps. No button contains '" ++ [character] ++ "'.")
 
 sentenceToTaps :: Phone -> String -> [(Digit, Presses)]
 sentenceToTaps phone = concat . map (characterToTaps phone)
 
 totalTapsInTapSequence :: [(Digit, Presses)] -> Presses
-totalTapsInTapSequence = foldr ((+) . snd) 0
+totalTapsInTapSequence = sum . map snd
 
 getButton :: Phone -> Char -> Maybe Button
 getButton (Phone buttons) character =
@@ -58,11 +60,38 @@ getButton (Phone buttons) character =
     _  -> Just $ head matches
   where
     matches = filter characterInButton buttons
-    characterInButton (Button label sequence) =
-      elem (toLower character) (sequence ++ [label])
+    characterInButton (Button label sequence) = elem character (sequence ++ [label])
 
 getLocationInSequence :: Char -> String -> Int
 getLocationInSequence character sequence =
-  case elemIndex (toLower character) sequence of
+  case elemIndex character sequence of
     (Just index) -> 1 + index
     Nothing      -> error ("Unable to find location of '" ++ [character] ++ "' in '" ++ sequence ++ "'.")
+
+-- Part 2
+
+mostFrequentLetter :: String -> Maybe [Char]
+mostFrequentLetter = mostFrequent
+
+mostFrequentWord :: String -> Maybe [String]
+mostFrequentWord = mostFrequent . words
+
+mostFrequentLetterInConversation :: [String] -> Maybe [Char]
+mostFrequentLetterInConversation = mostFrequent . concat
+
+mostFrequentWordInConversation :: [String] -> Maybe [String]
+mostFrequentWordInConversation = mostFrequent . concat . map words
+
+mostFrequent :: Eq a => [a] -> Maybe [a]
+mostFrequent [] = Nothing
+mostFrequent xs = Just (map fst $ filter ((== greatestFrequency) . snd) frequencies)
+  where
+    frequencies = frequencyList xs
+    greatestFrequency = maximum $ map snd frequencies
+
+frequencyList :: Eq a => [a] -> [(a, Int)]
+frequencyList [] = []
+frequencyList (x:xs) = (x, 1 + count x xs) : (frequencyList $ filter (/= x) xs)
+
+count :: Eq a => a -> [a] -> Int
+count character = length . filter (== character)
