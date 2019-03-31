721867cfd73b2104b009e6aface6cfd709c11f84
jacksoncooper <jacksoncooper@users.noreply.github.com>
2019-03-31 15:54:50 -0400

    Add Chapter 14 "Make a Gen random generator for the datatype".

---
+++ haskell-book/src/Chapter14/Generators.hs
@@ -0,0 +1,19 @@
+-- Chapter Exercises, Page 572
+
+module Chapter14.Generators where
+
+import Test.QuickCheck
+
+data Fool = Fulse | Frue
+  deriving (Eq, Show)
+
+-- 1.
+
+foolGeneratorEqual :: Gen Fool
+foolGeneratorEqual = elements [Fulse, Frue]
+
+-- 2.
+
+foolGeneratorBiased :: Gen Fool
+foolGeneratorBiased = do
+  frequency [(1, return Frue), (2, return Fulse)]
