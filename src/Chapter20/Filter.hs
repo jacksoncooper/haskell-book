cbcda69f186c906fd55a8d7f3c4b7f51987712fd
jacksoncooper <jacksoncooper@users.noreply.github.com>
2019-05-20 20:34:39 -0400

    Add Chapter 20 chapter exercises.

---
+++ src/Chapter20/Filter.hs
@@ -0,0 +1,32 @@
+-- Page 820
+
+module Chapter20.Filter where
+
+filterFoldable :: (Applicative f, Foldable t, Monoid (f a)) =>
+                  (a -> Bool)
+               -> t a
+               -> f a
+filterFoldable aToBool foldableOfA = foldMap aToMonoid foldableOfA
+    where aToMonoid a = if aToBool a then pure a else mempty
+
+-- E.g.
+
+-- filterFoldable (<= 2) [1..4] :: [Integer]
+
+--                              ^^
+-- Must coerce the type here, otherwise Haskell doesn't know what Applicative
+-- instance to dispatch for 'f', and what Monoid instance to dispatch for 'f a'.
+
+-- foldMap aToMonoid [1..4] =
+-- foldr (mappend . aToMonoid) mempty [1..4]
+
+-- (mappend . aToMonoid) 4 mempty =
+-- mappend (aToMonoid 4) mempty
+
+-- aToMonoid 4 = if (4 <= 2) then pure 4 else mempty
+--             = if False then [4] else []
+--             = []
+
+-- mappend [] [] = []
+
+-- ...
