0db429021eeea4636781d07775d5710f9c1ea49b
jacksoncooper <jacksoncooper@users.noreply.github.com>
2019-07-15 12:26:51 -0700

    Add Chapter 23 chapter exercises.

---
+++ src/Chapter23/States.hs
@@ -0,0 +1,30 @@
+-- Page 890
+
+module Chapter23.States where
+
+import Chapter23.State
+
+-- 1.
+
+get :: Moi s s
+get = Moi $ \s -> (s, s)
+
+-- 2.
+
+put :: s -> Moi s ()
+put s = Moi $ const ((), s)
+
+-- 3.
+
+exec :: Moi s a -> s -> s
+exec (Moi sa) = snd . sa
+
+-- 4.
+
+eval :: Moi s a -> s -> a
+eval (Moi sa) = fst . sa
+
+-- 5.
+
+modify :: (s -> s) -> Moi s ()
+modify f = Moi $ \s -> ((), f s)
